b8dddbc54ddb676f67e063cf5d04967f6efebc9b
Eunice Sombo <assigraceeunice.sombo@ynov.com>
2023-12-11 05:07:15 -0500

    jeu

--- Jeu.cpp
+++ Jeu.cpp
@@ -1,10 +1,10 @@
 #include "Jeu.hpp"
 
 void Joueurs::afficher(){
-    cout << "Nom : " << nomJoueur << endl;
-    cout << "Vie : " << VieJoueur << endl;
-    cout << "Energie : " << EnergieJoueur << endl;
-    cout << "Attaque : " << AttaqueJoueur << endl;
+    cout << "Nom : " << nom << endl;
+    cout << "Vie : " << Vie<< endl;
+    cout << "Energie : " << Energie << endl;
+    cout << "Attaque : " << Attaque << endl;
 }
 
 void Joueurs::attaquer(Joueurs &cible){
@@ -20,17 +20,17 @@ bool Joueurs::estVivant(){
 }
 
 Joueurs::Joueurs(){
-    nomJoueur = "Inconnu";
-    VieJoueur = 0;
-    EnergieJoueur = 0;
-    AttaqueJoueur = 0;
+    nom = "Inconnu";
+    Vie = 0;
+    Energie = 0;
+    Attaque = 0;
 }
 
 Joueurs::Joueurs(string nom, int Vie, int Energie, int Attaque){
-    this->nom = nomJoueur;
-    this->Vie = VieJoueur;
-    this->Energie = EnergieJoueur;
-    this->Attaque = AttaqueJoueur;
+    this->nom = nom;
+    this->Vie = Vie;
+    this->Energie = Energie;
+    this->Attaque = Attaque;
 }
 
 Joueurs::~Joueurs(){
@@ -38,17 +38,17 @@ Joueurs::~Joueurs(){
 }
 
 Heros::Heros(){
-    nomHeros = "Inconnu";
-    VieHeros = 0;
-    EnergieHeros = 0;
-    AttaqueHeros = 0;
+    nom = "Inconnu";
+    Vie = 0;
+    Energie = 0;
+    Attaque = 0;
 }
 
 Heros::Heros(string nom, int Vie, int Energie, int Attaque){
-    this->nom = nomHeros;
-    this->Vie = VieHeros;
-    this->Energie = EnergieHeros;
-    this->Attaque = AttaqueHeros;
+    this->nom = nom;
+    this->Vie = Vie;
+    this->Energie = Energie;
+    this->Attaque = Attaque;
 }
 
 Heros::~Heros(){
@@ -56,10 +56,10 @@ Heros::~Heros(){
 }
 
 void Heros::afficher(){
-    cout << "Nom : " << nomHeros << endl;
-    cout << "Vie : " << VieHeros << endl;
-    cout << "Energie : " << EnergieHeros << endl;
-    cout << "Attaque : " << AttaqueHeros << endl;
+    cout << "Nom : " << nom << endl;
+    cout << "Vie : " << Vie<< endl;
+    cout << "Energie : " << Energie << endl;
+    cout << "Attaque : " << Attaque << endl;
 }
 
 void Heros::attaquer(Joueurs &cible){
@@ -67,7 +67,7 @@ void Heros::attaquer(Joueurs &cible){
 }
 
 void Heros::recevoirDegats(int degats){
-    VieHeros -= degats;
+    Vie -= degats;
 }
 
 bool Heros::estVivant(){
@@ -81,34 +81,34 @@ vilain::vilain(){
     Attaque = 0;
 }
 
-vilain::vilain()( nom, int Vie, int Energie, int Attaque){
-    this->nom = nomVilain;
-    this->Vie = VieVilain ;
-    this->Energie = EnergieVilain;
-    this->Attaque = AttaqueVilain;
+vilain::vilain(string nom, int Vie, int Energie, int Attaque){
+    this->nom = nom;
+    this->Vie = Vie ;
+    this->Energie = Energie;
+    this->Attaque = Attaque;
 }
 
 vilain::~vilain(){
     cout << "Destruction de " << nom << endl;
 }
 
-void Vilain::afficher(){
-    cout << "Nom : " << nomVilain << endl;
-    cout << "Vie : " << VieVilain << endl;
-    cout << "Energie : " << EnergieVilain << endl;
-    cout << "Attaque : " << AttaqueVilain << endl;
+void vilain::afficher(){
+    cout << "Nom : " << nom << endl;
+    cout << "Vie : " << Vie << endl;
+    cout << "Energie : " << Energie << endl;
+    cout << "Attaque : " << Attaque << endl;
 }
 
-void Vilain::attaquer(Joueurs &cible){
+void vilain::attaquer(Joueurs &cible){
     cible.recevoirDegats(Attaque);
 }
 
-void Vilain ::recevoirDegats(int degats){
-    VieVilain -= degats;
+void vilain::recevoirDegats(int degats){
+    Vie -= degats;
 }
 
-bool Vilain::estVivant(){
-    return VieVilain > 0;
+bool vilain::estVivant(){
+    return Vie > 0;
 }
 
 
