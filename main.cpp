f2892d99742abe78ff69f56b9de60718c5f34db8
Maxime Isidore <maxime.isidore@ynov.com>
2023-12-12 10:01:41 +0100

    finish jeu cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
 #include "Jeu.hpp"
+
+int main(){
+    MBuf buf;
+    std::cout.rdbuf(&buf);
+    Heros h("Heros", 100, 100, 10);
+    vilain v("vilain", 100, 100, 10);
+    h.afficher();
+    v.afficher();
+    h.attaquer(v);
+    v.attaquer(h);
+    h.afficher();
+    v.afficher();
+    return 0;
+}
